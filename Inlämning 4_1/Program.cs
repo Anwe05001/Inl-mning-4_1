b2a444d879141bfff9321d87ce4d8749eae8aa50
Pingvu <nhlpool27@gmail.com>
2023-11-30 12:26:24 +0100

    Add project files.

---
+++ Inlämning 4_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tal = 9;
+        while (tal != 30)
+        {
+            tal++;
+            Console.WriteLine(tal);
+            
+        }
+
+        tal = 201;
+        while(tal != 180)
+        {
+            tal--;
+            Console.WriteLine(tal);
+        }
+
+        tal = 950;
+        while (tal !=1400)
+        {
+            tal += 50;
+            Console.WriteLine(tal);
+        }
+
+        Console.ReadKey();
+    }
+}
